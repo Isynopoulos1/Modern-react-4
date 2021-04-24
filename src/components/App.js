b049def542f037e64ab51423d2365349fefd4e1b
Isela <isela.v.alarcon@gmail.com>
2021-04-24 13:20:25 +0200

    Showing forms to the user

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default App;

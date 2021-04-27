80c2bfa6fe6dde677168f5df30b59d16fe7c42a9
Isela <isela.v.alarcon@gmail.com>
2021-04-27 15:23:26 +0200

    Solving context function part 1 arrow function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,12 @@ import React from "react";
 
 class SearchBar extends React.Component {
   state = { term: "" };
-  onFormSubmit(event) {
+
+  onFormSubmit = event => {
     event.preventDefault();
 
     console.log(this.state.term);
-  }
+  };
 
   render() {
     return (

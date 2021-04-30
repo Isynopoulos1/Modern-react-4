2c492016748baa617501a440de481751107d68bd
Isela <isela.v.alarcon@gmail.com>
2021-04-30 09:22:00 +0200

    Creating custom clients

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = { images: [] };
 
   onSearchSubmit = async term => {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_TOKEN}`
+    const response = await unsplash.get(
+      "https://api.unsplash.com/search/photos",
+      {
+        params: { query: term }
       }
-    });
+    );
 
     this.setState({ images: response.data.results });
   };
